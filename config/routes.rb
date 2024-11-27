18e8883c750899746edf79928d8d8eda88d90e2b
梅林遥大 <2211140011r@ed.fuk.kindai.ac.jp>
2024-11-27 08:21:57 +0000

    file3

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
-  resources :users 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'restaurants#index'  # または別のルートパス
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :users
-  root "users#index"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  
+  # 他のルーティング
 end
