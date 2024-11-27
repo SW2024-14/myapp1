9daff83d20abc9f0632568ed36267abaec7ec471
Yuto <2211140036y@ed.fuk.kindai.ac.jp>
2024-11-27 08:17:51 +0000

    マージ競合を解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
-  
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # ユーザー登録
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :users
-  root "users#index"
+  # ログイン
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # ユーザー情報編集
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
+  root "users#new"
+
+  resources :users  # Defines routes for users controller
 end
